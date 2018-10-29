2ad138e3f3234a43f738638e66c7ed2a9c50d4e4
Francisco octano <franciscooctano@MacBook-Air-de-Benjamin.local>
2018-10-29 19:06:33 -0300

    lines

--- src/container/technicians/Register.jsx
+++ src/container/technicians/Register.jsx
@@ -31,39 +31,35 @@ import UserService from "../../http/service/UserService";
 import { email } from "../../config/const";
 
 class List extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       items: [],
-      value: ''
+      value: ""
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
-    nextItemId = 0;
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
+  nextItemId = 0;
 
-    makeItem() {
-      return {
-        id: this.nextItemId++,
-        value: this.state.value
-      };
-    }
-  
-    
-    addItemImmutably = () => {
-      this.setState({
-        items: [...this.state.items,this.makeItem()]
-      });
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  makeItem() {
+    return {
+      id: this.nextItemId++,
+      value: this.state.value
     };
+  }
 
-  
-  
+  addItemImmutably = () => {
+    this.setState({
+      items: [...this.state.items, this.makeItem()]
+    });
+  };
+
   // componentDidMount() {
   //   UserService.findAll().then(data =>
   //     this.setState({
@@ -84,73 +80,87 @@ class List extends Component {
                 <BreadcrumbItem>
                   <Link to="/home">Home</Link>
                 </BreadcrumbItem>
-                <BreadcrumbItem><Link to="/tecnicos">Tecnicos</Link></BreadcrumbItem>
+                <BreadcrumbItem>
+                  <Link to="/tecnicos">Tecnicos</Link>
+                </BreadcrumbItem>
                 <BreadcrumbItem active>Informe Instalacion</BreadcrumbItem>
               </Breadcrumb>
               <Card>
                 <CardHeader>
                   <CardTitle>Nuevo Informe | Instalación</CardTitle>
                   <Form>
-                  <Row form>
-                  <Col md={3}>
-                    <FormGroup>
-          <Label for="exampleSelect">Empresa</Label>
-          <Input type="select" name="select" id="exampleSelect">
-            
-          </Input>
-        </FormGroup>
-        </Col>         
-        <Col md={3}>  
-                    <FormGroup>
-          <Label for="exampleSelect">Instalacion</Label>
-          <Input type="select" name="select" id="exampleSelect">
-            
-          </Input>
-        </FormGroup>
-        </Col>       
-        <Col md={3}>  
-                    <FormGroup>
-          <Label for="exampleSelect">Supervisor</Label>
-          <Input type="select" name="select" id="exampleSelect">
-            
-          </Input>
-        </FormGroup>
-        </Col>
-        <Col md={10}> 
-        <FormGroup>
-          <Label for="exampleAddress2">Observaciones</Label>
-          <Input type="text" value={this.state.value} onChange={this.handleChange} />
-        </FormGroup>
-        </Col>
-        <Col md={2}> 
-        <Button onClick={this.addItemImmutably} block>Agregar</Button>
-        </Col>
-        </Row>
-                    </Form>
+                    <Row form>
+                      <Col md={3}>
+                        <FormGroup>
+                          <Label for="exampleSelect">Empresa</Label>
+                          <Input
+                            type="select"
+                            name="select"
+                            id="exampleSelect"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col md={3}>
+                        <FormGroup>
+                          <Label for="exampleSelect">Instalacion</Label>
+                          <Input
+                            type="select"
+                            name="select"
+                            id="exampleSelect"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col md={3}>
+                        <FormGroup>
+                          <Label for="exampleSelect">Supervisor</Label>
+                          <Input
+                            type="select"
+                            name="select"
+                            id="exampleSelect"
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col md={10}>
+                        <FormGroup>
+                          <Label for="exampleAddress2">Observaciones</Label>
+                          <Input
+                            type="text"
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col md={2}>
+                        <Button onClick={this.addItemImmutably} block>
+                          Agregar
+                        </Button>
+                      </Col>
+                    </Row>
+                  </Form>
                 </CardHeader>
                 <CardBody>
-                      <Table size="sm" striped hover responsive>
-                        <thead>
-                          <tr>
-                            <th>Titulo</th>
-                            <th>Accion</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {items.map(e => (
-                            <tr key={e.id}>
-                              <td >{e.value}</td>
-                              <td>
-                                <Link to={`/informe/detalle/${e.id}`}>
-                                  <Button color="primary" size="sm" outline>
-                                    ver :<FontAwesomeIcon icon="eye" />
-                                  </Button>
-                                </Link>{" "}
-                              </td>
-                            </tr>
-                          ))}
-                        </tbody>
-                      </Table>
+                  <Table size="sm" striped hover responsive>
+                    <thead>
+                      <tr>
+                        <th>Titulo</th>
+                        <th>Accion</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {items.map(e => (
+                        <tr key={e.id}>
+                          <td>{e.value}</td>
+                          <td>
+                            <Link to={`/informe/detalle/${e.id}`}>
+                              <Button color="primary" size="sm" outline>
+                                ver :<FontAwesomeIcon icon="eye" />
+                              </Button>
+                            </Link>{" "}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </Table>
                 </CardBody>
               </Card>
             </Col>
