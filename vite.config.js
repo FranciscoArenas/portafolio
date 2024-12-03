7ad094dfc6061708b29912fcfe0e94fb56fc089f
FranciscoArenas <francisco_arenas@live.cl>
2024-12-03 03:05:34 -0300

    Corregir la base dinámica para las páginas de GitHub y el desarrollo local

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const isGitHubPages = process.env.GITHUB_ACTIONS || mode === 'production'
 
   return {
-    base: isGitHubPages ? '/portafolio/' : '/', // Ruta base dinámica
+    base: isGitHubPages ? 'https://franciscoarenas.github.io/portafolio/' : '/', // Ruta base dinámica
     plugins: [vue(), vueDevTools()],
     resolve: {
       alias: {
