fffd7c2202707bc8a003d83db897da585c267693
FranciscoArenas <francisco_arenas@live.cl>
2024-12-03 03:11:50 -0300

    Corregir la base dinámica para las páginas de GitHub y el desarrollo local

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig(({ mode }) => {
-  const isGitHubPages = process.env.GITHUB_ACTIONS || mode === 'production'
-
-  return {
-    base: isGitHubPages ? 'https://franciscoarenas.github.io/portafolio/' : '/', // Ruta base dinámica
-    plugins: [vue(), vueDevTools()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src/assets', import.meta.url)),
-      },
+export default defineConfig({
+  base: '/portafolio/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src/assets', import.meta.url)),
     },
-  }
+  },
 })
